3689c7ee555a23a7e0be16e4d09f1540714d1496
Ilya Chalyshkou <ilya.chalyshkou@leverx.com>
2025-07-06 20:19:06 +0000

    test commit

--- webapp/Component.js
+++ webapp/Component.js
@@ -2,7 +2,7 @@ sap.ui.define(
     ["sap/suite/ui/generic/template/lib/AppComponent"],
     function (Component) {
         "use strict";
-
+        console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
         return Component.extend("app.Component", {
             metadata: {
                 manifest: "json"
